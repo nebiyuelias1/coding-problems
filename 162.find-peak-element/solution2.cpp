ce9b6001714b250bf07708d0cdc25a27e68f8d58
eliasn <nebiyuelias1@gmail.com>
2022-07-13 15:18:30 +0300

    Solve find peak element problem

---
+++ 162.find-peak-element/solution2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left  < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid+1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    vector<int> v{1, 2, 3, 1};
+    cout << sol.findPeakElement(v);
+    return 0;
+}
