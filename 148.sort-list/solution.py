1b015f9045600463ec927d8d615253f343605723
eliasn <nebiyuelias1@gmail.com>
2022-07-19 09:07:35 +0300

    Solve sort linked list problem

--- 148.sort-list/solution.py
+++ 148.sort-list/solution.py
@@ -6,43 +6,70 @@ class ListNode:
         self.next = next
 
 class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        if list1.val <= list2.val:
+            start = list1
+            prev = list1
+            main = list1.next
+            secondary = list2
+        else:
+            start = list2
+            prev = list2
+            secondary = list1
+            main = list2.next
+        
+        while True:
+            if secondary == None:
+                break
+            if main == None:
+                prev.next = secondary
+                break
+            
+            if main.val >= secondary.val:
+                new_secondary = secondary.next
+                prev.next = secondary
+                secondary.next = main
+                prev = secondary
+                secondary = new_secondary
+            else:
+                main = main.next
+                prev = prev.next
+                
+            
+                
+        return start
+    
     def rec(self, start: Optional[ListNode], end: Optional[ListNode]) -> Optional[ListNode]:
+        if start == end:
+            return start
+        
         slow = start
         fast = start
         
-        while fast != end and fast.next != end:
+        while fast != end and fast and fast.next != end:
             slow = slow.next
             fast = fast.next.next
             
-        if start == end:
-            return
-            
-        self.rec(start, slow)
-        self.rec(slow.next, end)
+        new_next = slow.next
+        slow.next = None
+        if end:
+            end.next = None
+        left_start = self.rec(start, slow)
+        right_start = self.rec(new_next, end)
         
-        left_start = start
-        right_start = slow.next
+        ans = self.mergeTwoLists(left_start, right_start)
+        return ans
         
-        while True:
-            if not left_start or not right_start:
-                break
-            
-            if left_start == slow.next:
-                break
-                
-            if left_start.val > right_start.val:
-                temp = left_start.val
-                left_start.val = right_start.val
-                right_start.val = temp
-                left_start = left_start.next
-            else:
-                right_start = right_start.next
                 
             
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        self.rec(head, None)
-        
-        return head
+        ans = self.rec(head, None)
+        return ans
     
 three = ListNode(val=3)
 one = ListNode(val=1, next=three)
