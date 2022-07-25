62ae1d2c3689ecfd5de641333e17dbca46f87ec1
eliasn <nebiyuelias1@gmail.com>
2022-07-25 14:41:17 +0300

    Solve product of array except self problem

--- 238.product-of-array-except-self/solution.py
+++ 238.product-of-array-except-self/solution.py
@@ -6,13 +6,24 @@ class Solution:
         n = len(nums)
         ans = [1] * n
 
-        for i in range(n):
-            for j in range(n):
-                if i != j:
-                    ans[i] = ans[i] * nums[j]
-
+        for i in range(1, n):
+            ans[i] = ans[i-1] * nums[i-1]
+        
+        temp = 1
+        for i in range(n-2, -1, -1):
+            temp = temp * nums[i+1]
+            ans[i] = ans[i] * temp
+            
         return ans
 
 
 sol = Solution()
 sol.productExceptSelf([1, 2, 3, 4])
+
+# [1, 2, 3, 4]
+# prod = [1, 1, 2, 6]
+# temp = 1
+# prod = [24, 12, 8, 6]
+
+# suffix: [24, 12, 4, 1]
+# ans: [24, 12, 8, 6]
